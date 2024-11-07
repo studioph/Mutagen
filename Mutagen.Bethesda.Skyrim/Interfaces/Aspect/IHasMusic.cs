4f9c3a319b0a8bf1687f157c097d0b4b34a92e77
Paul Hutchings <dev@studiop.page>
2024-11-07 00:34:59 -0500

    Use new custom aspect attribute

--- Mutagen.Bethesda.Skyrim/Interfaces/Aspect/IHasMusic.cs
+++ Mutagen.Bethesda.Skyrim/Interfaces/Aspect/IHasMusic.cs
@@ -1,10 +1,12 @@
 using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.SourceGenerators;
 
 namespace Mutagen.Bethesda.Skyrim;
 
 /// <summary>
 /// Common interface for records with a MusicType
 /// </summary>
+[CustomAspectInterface(typeof(ICell), typeof(IWorldspace))]
 public interface IHasMusic : ISkyrimMajorRecordInternal, IHasMusicGetter
 {
     IFormLinkNullable<IMusicTypeGetter> Music { get; }
@@ -13,6 +15,7 @@ public interface IHasMusic : ISkyrimMajorRecordInternal, IHasMusicGetter
 /// <summary>
 /// Common interface for records with a MusicType
 /// </summary>
+[CustomAspectInterface(typeof(ICell), typeof(ICellGetter), typeof(IWorldspace), typeof(IWorldspaceGetter))]
 public interface IHasMusicGetter : ISkyrimMajorRecordGetter
 {
     IFormLinkNullableGetter<IMusicTypeGetter> Music { get; }
